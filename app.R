a216d0e6778df3eb24a6e6afd488c453c32ba41d
davelovellCARU <david.lovell@churcharmy.org>
2020-01-10 11:41:27 +0000

    removed surplus code

--- app.R
+++ app.R
@@ -54,25 +54,3 @@ server = function(input, output) {
 
 # ShinyApp ------------------------------------------------------------------
 shinyApp(ui = ui, server = server)
-
-
-# Unused code
-# 
-# statsTable = eventReactive(input$bigButton,
-#                            {excelUrl = "https://www.dropbox.com/s/v9lm9y7nso8yw1x/dashboardTotals.xlsx?dl=1&raw=1"
-#                            
-#                            tmpF <- paste0(getwd(), "/newTing.xlsx")
-#                            theFile <- url(excelUrl, open = "rb")
-#                            
-#                            binary = readBin(theFile, raw(), 100000)
-#                            
-#                            writeBin(binary, tmpF)
-#                            
-#                            (readxl::read_excel(tmpF)) %>%
-#                              rename(surveys = 1,
-#                                     interviews = 2,
-#                                     focusGroups = 3,
-#                                     focusGroupParticipants = 4,
-#                                     researchersTrained = 5) -> tmp
-#                            return(tmp)
-#                            })
